524ad9d0d636c8acb8e9f9d693e2d81e0b119b65
christinacrain72-alt <christinacrain72@gmail.com>
2025-08-30 23:39:28 -0400

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className="text-pink-600 text-3xl font-bold p-6">
+      Lit by 2 Website is working!
+    </h1>
+  )
+}
